727d13a441fcc42ffaac791b5cd509856b029fb8
nagibinstudio <nagibinstudio@gmail.com>
2023-06-26 15:32:33 +0300

    i

--- src/components/Courses/CoursesBlock.tsx
+++ src/components/Courses/CoursesBlock.tsx
@@ -4,6 +4,13 @@ import {Link} from "react-router-dom";
 const CoursesBlock: React.FC = () => {
     return (
         <div className="courses-block">
+            <div
+                className="courses-block-image"
+                style={{
+                    backgroundImage:
+                        "url('https://images.unsplash.com/photo-1687360464268-09429aecd6bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80')",
+                }}
+            ></div>
             <div className="courses-block-content">
                 <div className="courses-block-content-text">
                     <p className="courses-block-content-text__subtitle">
@@ -21,24 +28,13 @@ const CoursesBlock: React.FC = () => {
                 <div className="courses-block-content-btn">
                     <h3 className="courses-block-content-btn__price">
                         от 1094₽
-					</h3>
-					
-                    <Link
-                        to="/"
-                        className="btn courses-block-content-btn__btn"
-                    >
+                    </h3>
+
+                    <Link to="/" className="btn courses-block-content-btn__btn">
                         Подробнее
                     </Link>
                 </div>
             </div>
-
-            <div
-                className="courses-block-image"
-                style={{
-                    backgroundImage:
-                        "url('https://images.unsplash.com/photo-1687360464268-09429aecd6bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80')",
-                }}
-            ></div>
         </div>
     );
 };
